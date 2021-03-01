26f51cad9e144a472afdda8476e2f2fb45e8d5cb
ULYSSESS <ULYSSESS@DESKTOP-MK9933E>
2021-03-01 11:08:53 -0400

    Se instalo el EntityFrameworkCore.SQLite y el EntityFrameworkCore.Tools.

---
+++ Registro_Detalles_Suplidores_Blazor/Models/Suplidores.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Registro_Detalles_Suplidores_Blazor.Models
+{
+    public class Suplidores
+    {
+
+    }
+}
